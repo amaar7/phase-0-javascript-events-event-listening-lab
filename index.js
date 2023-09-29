bfdd6e241066cbce9a62751107f664a1cd015afb
amaar <amaar.ahmednur@student.moringaschool.com>
2023-09-29 14:56:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+
+function clickAlert(){
+    alert('I was clicked');
 }
+
+if (input) {
+    input.addEventListener('click', clickAlert);
+    return true;  
+  } else {
+    return false; 
+  }
+}
+addingEventListener();
+
